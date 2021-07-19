e665283dc6ad413802c8ae7ffcb8445519ed5238
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-07-19 16:07:00 +1200

    Parameterized / Arguments TestFixtures

---
+++ TestProject1/Tests/CustomerOrderServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace TestProject1.Tests
+{
+    [TestFixture(CustomerType.Premium, 100.00)]
+    [TestFixture(CustomerType.Basic)]
+    public class CustomerOrderServiceTests
+    {
+        private CustomerType customerType;
+        private double minOrder;
+ 
+        public CustomerOrderServiceTests(CustomerType customerType, double minOrder)
+        {
+            this.customerType = customerType;
+            this.minOrder = minOrder;
+        }
+ 
+        public CustomerOrderServiceTests(CustomerType customerType) : this(customerType, 0)
+        {
+        }
+ 
+        [TestCase]
+        public void TestMethod()
+        {
+            Assert.IsTrue((customerType == CustomerType.Basic && minOrder == 0 || customerType == CustomerType.Premium && minOrder > 0));
+        }
+    }
+}
--- TestProject1/Tests/UnitTest1.cs
+++ TestProject1/Tests/UnitTest1.cs
@@ -31,10 +31,10 @@ namespace TestProject1.Tests
                 };
 
                 var customerOrderService = new CustomerOrderService();
-                
+
                 //Act
                 customerOrderService.ApplyDiscount(premiumCustomer, order);
-                
+
                 //Assert
                 Assert.AreEqual(order.Amount, 135);
             }
