6f12f9425b85f462ad40331675bfbc80d7abcda0
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-07-19 17:42:10 +1200

    Generic TestFixture

--- TestProject1/Tests/CustomerOrderServiceTests.cs
+++ TestProject1/Tests/CustomerOrderServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace TestProject1.Tests
 {
@@ -8,21 +9,42 @@ namespace TestProject1.Tests
     {
         private CustomerType customerType;
         private double minOrder;
- 
+
         public CustomerOrderServiceTests(CustomerType customerType, double minOrder)
         {
             this.customerType = customerType;
             this.minOrder = minOrder;
         }
- 
+
         public CustomerOrderServiceTests(CustomerType customerType) : this(customerType, 0)
         {
         }
- 
+
         [TestCase]
         public void TestMethod()
         {
-            Assert.IsTrue((customerType == CustomerType.Basic && minOrder == 0 || customerType == CustomerType.Premium && minOrder > 0));
+            Assert.IsTrue((customerType == CustomerType.Basic && minOrder == 0 ||
+                           customerType == CustomerType.Premium && minOrder > 0));
+        }
+    }
+
+    [TestFixture(CustomerType.Premium, 100.00, TypeArgs = new Type[] {typeof(CustomerType), typeof(double)})]
+    public class CustomerOrderServiceTests<T1, T2>
+    {
+        private T1 customerType;
+        private T2 minOrder;
+
+        public CustomerOrderServiceTests(T1 customerType, T2 minOrder)
+        {
+            this.customerType = customerType;
+            this.minOrder = minOrder;
+        }
+
+        [TestCase]
+        public void TestMethod()
+        {
+            Assert.That(customerType, Is.TypeOf<CustomerType>());
+            Assert.That(minOrder, Is.TypeOf<double>());
         }
     }
 }
