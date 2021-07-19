dc887992e7294bf72bc1433020c6135d6f2d8127
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-07-19 17:26:39 +1200

    NUnit TestFixture Inheritance

---
+++ TestProject1/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace TestProject1
+{
+    public class Employee
+    {
+        protected internal string Name { get; set; }
+
+        public bool ContainsIllegalChars()
+        {
+            return Name.Contains("$");
+        }
+    }
+    public class Manager : Employee {}
+    public class DeliveryManager : Employee {}
+}
+
---
+++ TestProject1/Tests/EmployeeTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace TestProject1.Tests
+{
+    [TestFixture]
+    public class EmployeeTests
+    {
+        public virtual Employee CreateEmployee()
+        {
+            return new Employee();
+        }
+
+        [TestCase]
+        public void When_NameContainsIllegalChars_Expect_ContainsIllegalChars_ReturnsTrue()
+        {
+            var employee = CreateEmployee();
+            employee.Name = "Da$ya";
+
+            var result = employee.ContainsIllegalChars();
+
+            Assert.IsTrue(result);
+        }
+    }
+    public class ManagerTests : EmployeeTests
+    {
+        public override Employee CreateEmployee()
+        {
+            return new Manager();
+        }
+    }
+ 
+    public class VicePresidentTests : EmployeeTests
+    {
+        public override Employee CreateEmployee()
+        {
+            return new DeliveryManager();
+        }
+    }
+}
